42cc49d91fd43806af68a2772c6491149026c30e
Alex8aGry <l17420589@jiquilpan.tecnm.mx>
2021-04-23 22:34:58 -0500

    IDK

---
+++ src/Components/Products.jsx
@@ -0,0 +1,18 @@
+import react from 'react'
+
+const Products=()=>{
+    return(
+
+        <div className="Products">
+           
+            <div className="Produc">
+        Producto 1
+           </div>
+           
+        </div>
+      
+
+    )
+
+}
+export default Products
