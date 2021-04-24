008192c39ea50ece1cfa2707476eba2e53481c11
Alex8aGry <l17420589@jiquilpan.tecnm.mx>
2021-04-24 11:23:53 -0500

    MChanges

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -3,14 +3,13 @@ import react from 'react'
 const Opc=["P1","P2","P3","P4","P5","P6"];
 
 
-
-
 const renderImgs  =(onClickImg)=>{   
     const renderImg =(name) =>(
       <div className="Product"> 
-      <div className="proof">
+     
         <img src={"./Imgs/"+name+".png"} className="ImgsP" alt="Imagen"></img>
-        <button className="btnVer">ver Detalles</button>
+        <div className="proof">
+        <button className="btnVer">Ver Detalles</button>
         </div>
         </div>
     
