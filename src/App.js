c1a1457dcb9e48c4e7c6a3167e5697c917812b66
Luis Fernando Sanchez Cazares <l17420613@jiquilpan.tecnm.mx>
2021-04-23 20:48:53 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
           Hola Mundo
           Esto es una simulacion
+          Si funciono
         </p>
         <a
           className="App-link"
