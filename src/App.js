cad3a23f4d002957156c7de5462651bf48a6ea00
Luis Fernando Sanchez Cazares <l17420613@jiquilpan.tecnm.mx>
2021-04-24 23:14:41 -0500

    .

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
 import Header from './Components/Header';
-import Products from './Components/Products'
-import Footer from './Components/Footer'
-import FooterSm from './Components/FooterSm'
-import './Style/Bisuteria.css'
+import Slider from './Components/Slider';
+import Products from './Components/Products';
+import Footer from './Components/Footer';
+import FooterSm from './Components/FooterSm';
+import './Style/Bisuteria.css';
 
 
 function App() {
   return (
     <div className="App">
           <Header/>
+          <Slider/>
           <Products/>
           <Footer/>
           <FooterSm/>
---
+++ src/Components/Slider.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Slider = () => {
+    return(
+        <div className="slider">
+			<ul>
+				<li>
+                    <img src="./Imgs/slider1.jpg"/>
+                </li>
+				<li>
+                    <img src="./Imgs/slider2.jpg"/>
+                </li>
+				<li>
+                    <img src="./Imgs/slider1.jpg"/>
+                </li>
+				<li>
+                    <img src="./Imgs/slider2.jpg"/>
+                </li>
+			</ul>
+		</div>
+    );
+}
+
+export default Slider
