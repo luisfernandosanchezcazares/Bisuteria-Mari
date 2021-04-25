0f4c12176361cc6a6b10105f96781d438e26e1fb
Luis Fernando Sanchez Cazares <l17420613@jiquilpan.tecnm.mx>
2021-04-24 21:38:14 -0500

    .

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,18 +1,34 @@
 import React from 'react';
 import Button from './Button';
 
+
+
 const Header = () => {
     return(
         <div className="header">
             <div className="headerUp">
-                <img src="./Imgs/logo.png" className="cajaLogo"/>
+                {/* <div className="contenedorLogo"> */}
+                    <img src="./Imgs/logo.png" className="Logo"/>
+                    <p>AQUI VA EL ESLOGAN</p>
+                {/* </div> */}
                 <div className="cajaBusqueda">
                     <input className="cuadroTexto" placeholder="Ingresa el nombre del producto"/>
                     <Button type="botonBuscar" text="Buscar" />
                 </div>
             </div>
             <div className="headerDown">
-
+                    <ul className="nav">
+                        <li><a href="">Inicio</a></li>
+                        <li><a href="">Categorias</a>
+                            <ul>
+                                <li><a href="">Collares</a></li>
+                                <li><a href="">Aretes</a></li>
+                                <li><a href="">Pulseras</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="">Contacto</a></li>
+                        <li><a href="">Acerca De</a></li>
+                    </ul>
             </div>
         </div>
     );
