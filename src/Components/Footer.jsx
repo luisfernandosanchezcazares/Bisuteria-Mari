9e116edc2d64cd756acc266c2ff9ed4a850e1440
Alex8aGry <l17420589@jiquilpan.tecnm.mx>
2021-04-25 21:45:27 -0500

    Dry

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,6 +1,36 @@
 import react from 'react'
 
-const Footer=()=>{
+
+
+
+const Opc=["WhatsApp","Envelope"];
+const Titles=["","","351-112-45-23","someone@.com"]
+let ind=0;
+
+const renderImgs  =(onClickImg)=>{  
+    
+    const renderImg =(name) =>(
+        <li className="Contact">
+        <p className="Detail">
+       
+      <img src={"./Imgs/ImgsSocialM/"+name+".png"} key={name} className="SocialImg"></img>
+    
+      
+      {Titles[ind++]}
+      
+       
+          
+      </p>
+       </li>
+   
+    
+    )
+    return Opc.map(renderImg)
+    
+}
+    
+
+const Footer=(onClickImg)=>{
     return(
 
         <div className="Footer">
@@ -9,23 +39,19 @@ const Footer=()=>{
            Contacto
           <hr></hr>
            <ul>
-           <li className="Contact">
-            <p className="Detail">
-          <img src="./Imgs/ImgsSocialM/WhatsApp.png"  className="SocialImg"></img>
-          351-117-25-28</p>
-           </li>
+          
+          {renderImgs(onClickImg)}
 
-           <li className="Contact">
-               <p className="Detail">
-          <img src="./Imgs/ImgsSocialM/Envelope.png"  className="SocialImg"></img>
-          okay@someone.com</p>
-           </li>
-           <li className="Contact">
-          <p className="Detail">     
-          <img src="./Imgs/ImgsSocialM/Location.png"  className="SocialImgL"></img>
-          Some known spot</p>
-           </li>
 
+          <li className="Contact">
+        <p className="Detail">
+       
+      <img src="./Imgs/ImgsSocialM/Location.png"  className="SocialImgL"></img>
+        Domicilio Conocido
+       
+          
+      </p>
+       </li>
            </ul>
                       </div>
 
