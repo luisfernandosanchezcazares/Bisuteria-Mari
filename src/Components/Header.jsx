1a68024001a7c6b373e5001991602840ab487112
Luis Fernando Sanchez Cazares <l17420613@jiquilpan.tecnm.mx>
2021-06-03 16:19:47 -0500

    *

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import Button from './Button';
+import Grid from '@material-ui/core/Grid'
 
 const Header = () => {
     return(
-        <div className="header">
-            <div className="headerUp">
-                    <img src="./Imgs/logo.png" className="Logo"/>
+        <Grid className="header" container justify = "center" column spacing = {2}>
+            <Grid className="headerUp" container item xs = {12}>
+                <Grid container justify = "left" container item xs = {4}>
+                    <img src="./Imgs/logo.png"/>
                     <p>AQUI VA EL ESLOGAN</p>
-                <div className="cajaBusqueda">
+                </Grid>
+                <div>
                     <input className="cuadroTexto" placeholder="Ingresa el nombre del producto"/>
                     <Button type="botonBuscar" text="Buscar" />
                 </div>
-            </div>
-            <div className="headerDown">
+            </Grid>
+            <Grid className="headerDown" container item xs = {12}>
                     <ul className="nav">
                         <li><a href="">Inicio</a></li>
                         <li><a href="">Categorias</a>
@@ -25,8 +28,8 @@ const Header = () => {
                         <li><a href="">Contacto</a></li>
                         <li><a href="">Acerca De</a></li>
                     </ul>
-            </div>
-        </div>
+            </Grid>
+        </Grid>
     );
 }
 
