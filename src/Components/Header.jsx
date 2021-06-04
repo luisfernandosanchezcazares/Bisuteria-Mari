0fc7c09d87bcd4a598bb1461291380e45bd6c6d3
Luis Fernando Sanchez Cazares <l17420613@jiquilpan.tecnm.mx>
2021-06-03 20:54:46 -0500

    Header Responsive

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,35 +1,17 @@
 import React from 'react';
 import Button from './Button';
 import Grid from '@material-ui/core/Grid'
-
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-// import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/core/Menu';
+import Typography from '@material-ui/core/Typography'
 
 const Header = () => {
     return(
-        // <AppBar position="static">
-        //     <Toolbar>
-        //         <IconButton edge="start"  color="inherit" aria-label="menu">
-        //             <MenuIcon />
-        //         </IconButton>
-        //         <Typography variant="h6">
-        //             News
-        //          </Typography>
-        //         <Button color="inherit">Login</Button>
-        //     </Toolbar>
-        // </AppBar>
         <Grid className="header" container justify = "center" column spacing = {2}>
-            <Grid className="headerUp" container item xs = {12}>
-                <Grid container justify = "left" container item xs = {4} md = {6} lg = {6}>
+            <Grid className="headerUp" container item xs = {12} sm = {12} md = {12} lg = {12}>
+                <Grid container justify = "left" container item xs = {12} sm = {4} md = {6} lg = {6}>
                     <img height="50px" src="./Imgs/logo.png" className="Logo"/>
-                    <p>Luce elegante, luce genial</p>
+                    <p className ="eslogan">Luce elegante, luce genial</p>
                 </Grid>
-                <Grid container item xs = {8} md = {6} lg = {6}>
+                <Grid container item xs = {12} sm = {8} md = {6} lg = {6}>
                     <div className="cajaBusqueda">
                         <input className="cuadroTexto" placeholder="Ingresa el nombre del producto"/>
                         <Button type="botonBuscar" text="Buscar" />
