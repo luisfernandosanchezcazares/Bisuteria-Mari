1d7c1a8673d0577c7c4483aa493cf8e77bfb72a3
Alex8aGry <l17420589@jiquilpan.tecnm.mx>
2021-06-10 20:54:37 -0500

    idk

---
+++ src/Pages/Details.jsx
@@ -0,0 +1,27 @@
+import Header from '../Components/Header';
+import Footer from '../Components/Footer';
+import FooterSm from '../Components/FooterSm';
+import Slider from '../Components/Slider';
+import Products from '../Components/Products';
+import DetailsImg from '../Components/DetailsImg'
+import '../Style/Bisuteria.css';
+import App from './AddProduct'
+
+
+const Details = () => {
+    
+  return (
+    <>
+  
+     <Header></Header>
+     <DetailsImg name={localStorage.getItem("value")} ></DetailsImg>
+
+    <Footer></Footer>
+    <FooterSm></FooterSm>
+   {/* <App></App>*/}
+     
+        </>
+  );
+}
+
+export default Details;
