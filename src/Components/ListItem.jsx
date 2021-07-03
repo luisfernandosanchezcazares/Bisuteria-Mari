2df5102b307c1ac6e21e0372e5e54208e2002d8f
Luis Fernando Sanchez Cazares <l17420613@jiquilpan.tecnm.mx>
2021-07-02 23:49:28 -0500

    Skeleton add

--- src/Components/ListItem.jsx
+++ src/Components/ListItem.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Button from './Button'
 import Skeleton from '@material-ui/lab/Skeleton'
 
 const ListItem = ({item}) => {
+
+    const [loader, setLoader] = useState(false)
+
+    useEffect(() => setTimeout(() => setLoader(true), 1000), [])
+
     return(
         <div className="Product" >
             {
-                item.url ?
+                loader ?
                     <img src={item.url} className="ImgsP" key={item.url} alt="Imagen" />
                 :
-                    <Skeleton variant="rect" height={300}  width={300} />
+                    <Skeleton variant="rect" height={300}  width={300} />   
             }
             <div className="proof">
                 <Button type="btnVer" text="Ver Detalles" name={item.url} id = {item.id}/>
